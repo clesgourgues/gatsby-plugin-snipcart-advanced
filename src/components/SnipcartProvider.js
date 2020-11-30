0f4039f3398fe0aeacf8611d24086ecc3fd434b9
cecile lesgourgues <cecile.lesgourgues@ekino.com>
2020-11-30 21:37:04 +0100

    add locale arg to changeLanguage

--- src/components/SnipcartProvider.js
+++ src/components/SnipcartProvider.js
@@ -1,26 +1,26 @@
 const React = require('react');
-const {useStore, SnipcartContext} = require('../store');
+const { useStore, SnipcartContext } = require('../store');
 
 /**
  * @param props : {currency, version}
  */
-const SnipcartProvider = props => {
+const SnipcartProvider = (props) => {
   const [state, dispatch] = useStore();
-  const {defaultLang, locales} = props;
-  const changeLanguage = lang => {
-    const lng = locales[defaultLang] || {};
+  const { defaultLang, locales } = props;
+  const changeLanguage = (lang, locale) => {
+    const lng = locales[locale] || {};
     window.Snipcart.api.session.setLanguage(lang, lng);
   };
   React.useEffect(() => {
     const listenSnipcart = () => {
       document.addEventListener('snipcart.ready', () => {
-        dispatch({type: 'setReady', payload: true});
+        dispatch({ type: 'setReady', payload: true });
         changeLanguage(defaultLang);
       });
     };
 
     if (window.Snipcart !== undefined) {
-      dispatch({type: 'setReady', payload: true});
+      dispatch({ type: 'setReady', payload: true });
       changeLanguage(defaultLang);
     } else {
       listenSnipcart();
@@ -28,7 +28,7 @@ const SnipcartProvider = props => {
   }, [props, dispatch, defaultLang, locales]);
 
   return (
-    <SnipcartContext.Provider value={{state, changeLanguage}}>
+    <SnipcartContext.Provider value={{ state, changeLanguage }}>
       {props.children}
     </SnipcartContext.Provider>
   );
